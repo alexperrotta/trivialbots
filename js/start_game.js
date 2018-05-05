7c6bb61a3f5aaeb4218d8a567b01cee94340b975
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-05-04 21:16:28 -0400

    added dropdown menu

---
+++ js/start_game.js
@@ -0,0 +1,25 @@
+// Create drop down menu
+
+class ChooseCategory {
+	constructor(dropdown) {
+		this.container = $(container);
+
+		this.render();
+	}
+
+	render() {
+		this.container.html(`
+			<div class="dropdown">
+			<a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+     		Dropdown link</a>
+     		<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+     		<a class="dropdown-item" href="#">Category 1</a>
+     		<a class="dropdown-item" href="#">Category 2</a>
+     		<a class="dropdown-item" href="#">Category 3</a>
+   			</div>
+ 			</div>
+			`); 
+	}
+
+}
+
